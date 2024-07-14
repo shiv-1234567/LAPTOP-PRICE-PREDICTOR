511bc9f864d751b8db66bb83d6bd888fb8ca9690
Shivendra prasad Mishra <shivendramishra346@gmail.com>
2024-07-14 17:31:54 +0530

    Update PRICE-PREDICTOR.py

--- PRICE-PREDICTOR.py
+++ PRICE-PREDICTOR.py
@@ -63,4 +63,4 @@ if st.button('Predict Price'):
     query = np.array([company, type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os],dtype=object)
 
     query = query.reshape(1, 12)
-    st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
+    st.title("The predicted price of this laptop for this combination is " + str(int(np.exp(pipe.predict(query)[0]))))
